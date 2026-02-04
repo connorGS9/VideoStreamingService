c300e477b567c29439f8482cee1eb9d272ddf829
connorGS9 <cs36356@georgiasouthern.edu>
2026-02-03 22:39:20 -0500

    Initial commit of java application 'Backend' file with application start point 'VideoStreamingApplication.java'.

---
+++ Backend/src/main/java/com/connorGS9/videoStreamingService/VideoStreamingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.connorGS9.videoStreamingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingServiceApplication {
+    // Entry point
+    public static void main(String[] args) {
+        SpringApplication.run(VideoStreamingServiceApplication.class, args);
+    }
+
+
+}
