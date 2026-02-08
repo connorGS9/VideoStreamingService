0a37eff726d33dcbf24d8bd489fa0254d04be9ec
connorGS9 <cs36356@georgiasouthern.edu>
2026-02-08 15:39:45 -0500

    Created the StorageService S3 wrapper, upload DTO, and exception that accompany it.

---
+++ Backend/src/main/java/com/connorGS9/videoStreamingService/dto/VideoUploadRequest.java
@@ -0,0 +1,41 @@
+package com.connorGS9.videoStreamingService.dto;
+
+
+public class VideoUploadRequest {
+    private String title;
+    private String description;
+    private Long userId;
+
+    public VideoUploadRequest() {
+    }
+
+    public VideoUploadRequest(String title, String description, Long userId) {
+        this.title = title;
+        this.description = description;
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
